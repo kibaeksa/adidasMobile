0c295e66e5defa98f50a1c727108b11a92cbcf14
Seoyeonggyu <syg0713@gmail.com>
2019-07-05 10:33:56 +0900

    ree_FRT_gnb 테스트

--- mobile/designjs/reebok/efu2018/ui.js
+++ mobile/designjs/reebok/efu2018/ui.js
@@ -105,7 +105,7 @@ jQuery(function(){
 		jQuery("#brand_navWrap, #global-search-slide").removeClass("active");
 		jQuery(".searchBtn a, .brandmenuClose").removeClass("on");
 		jQuery("#wrap, .menuWrap").height(jQuery(window).height())
-		$('#wrap .menuWrap').removeClass('slide-open').css({
+		$('#wrap .menuWrap').css({
 			transform : 'translate3d(0 ,0 ,0)'
 		});
 		e.preventDefault();
